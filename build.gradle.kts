5453a97bffd6774fa4be46848108b4aa23c99800
topanim <gorogannisan641@gmail.com>
2024-07-26 13:37:08 +0300

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,23 @@
 plugins {
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.serialization") version "2.0.0"
+    id("maven-publish")
 }
 
-group = "org.maestri"
-version = "1.0-SNAPSHOT"
+group = "com.github.topanim"
+version = "1.1"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
-    testImplementation(kotlin("test"))
+    implementation("com.github.jitpack:gradle-simple:2.0")
+    implementation("com.google.guava:guava:32.0.0-android")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(20)
 }
